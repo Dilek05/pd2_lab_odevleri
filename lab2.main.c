b521626a246e7da25c55b3c3d778841c93816055
Dilek Ünlü <146916274+Dilek05@users.noreply.github.com>
2024-03-29 02:10:00 +0300

    Add files via upload

---
+++ lab2.main.c
@@ -0,0 +1,206 @@
+//
+//  main.c
+//  pd2_lab2
+//
+//  Created by Dilek ÜNLÜ on 22.02.2024.
+//
+
+  
+#include <stdio.h>
+
+
+/*void hesapla(int ogr_sayisi, int gecme_not)
+{
+    int not[100], i;
+    int kalan_ogr_sayisi=0;
+    float ortalama=0;
+    
+    for(i=0; i < ogr_sayisi; i++) // dizilerin indeksleri 0 dan başlar unutmaa
+    {
+        printf("%d.öğrencinin notunu giriniz:",i+1);
+        scanf("%d", &not[i]);
+    }
+    
+    for (i=0; i< ogr_sayisi; i++)
+    {
+        ortalama= ortalama + not[i];
+        
+        if (not[i] < gecme_not)
+        {
+            kalan_ogr_sayisi++;
+        }
+    }
+    ortalama = ortalama / ogr_sayisi;
+    printf("\n");
+    
+    printf("sınıfın ortalaması: %.2f\n", ortalama);
+    printf("kalan öğrenci sayısı: %d\n",kalan_ogr_sayisi);
+    
+}
+
+int main(void){
+    int ogr_sayisi ;
+    int gecme_not ;
+    
+    printf("öğrenci sayısını giriniz (en fazla 100): ");
+    scanf("%d", &ogr_sayisi);
+    
+    printf("geçme notunu giriniz: ");
+    scanf("%d", &gecme_not);
+    
+    hesapla(ogr_sayisi,gecme_not);
+    
+    return 0;
+}*/
+
+
+
+
+
+
+
+
+
+/*#include <stdio.h>
+
+void kitleindeks(void){
+    float boy,kilo;
+    float indeks=0;
+    
+    printf("boyunuzu giriniz (metre cinsinden) : ");
+    scanf("%f", &boy);
+    
+    printf("kilonuzu giriniz: ");
+    scanf("%f", &kilo);
+    
+    indeks = kilo / (boy * boy);
+    
+    if (0 <=indeks && indeks <19)
+        printf("durumu: ZAYIF\n");
+    else if (19 <=indeks && indeks <26)
+        printf("durumu: NORMAL\n");
+    else if (26 <=indeks && indeks <31)
+        printf("durumu: ! KİLOLU !\n");
+    else
+        printf("durumu: !! OBEZ !!\n");
+}
+
+int main(void)
+{
+    kitleindeks();
+    
+    return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
